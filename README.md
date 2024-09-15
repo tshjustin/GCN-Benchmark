32c8f6c26786a65cfda5edc137914de828620fe3
tshjustin <j.tann2305@gmail.com>
2024-09-15 12:46:59 +0800

    updated dataloder

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ pip install -r requirements.txt
 1. Trying out different parameters  (Learning rates, etc)
 
 2. Add visuals of performances 
+
+
+### CORA Data 
+The Cora dataset consists of 2708 scientific publications classified into one of seven classes. The citation network consists of 5429 links. Each publication in the dataset is described by a 0/1-valued word vector indicating the absence/presence of the corresponding word from the dictionary. The dictionary consists of 1433 unique words.
--- src/dataloader.py
+++ src/dataloader.py
@@ -21,10 +21,8 @@ def train_test_val_split(labels, num_classes, config):
     return train_set, validation_set, test_set
 
 
-def enumerate_labels(labels):
-    """ Converts the labels from the original
-        string form to the integer [0:MaxLabels-1]
-    """
+def map_labels(labels):
+    "Map string labels to a numeric value"
     unique = sorted(set(labels)) 
     label_map = {label: idx for idx, label in enumerate(unique)}
     labels = np.array([label_map[label] for label in labels])
@@ -32,20 +30,53 @@ def enumerate_labels(labels):
 
 
 def normalize_adjacency(adj):
-    """Normalizes the matrix"""
-    adj = adj + sparse.eye(adj.shape[0])
+    """normalization of adj matrix."""
+    identity_matrix = sparse.eye(adj.shape[0])
+    adj_with_self_loops = adj + identity_matrix
 
-    node_degrees = np.array(adj.sum(1))
-    node_degrees = np.power(node_degrees, -0.5).flatten()
-    node_degrees[np.isinf(node_degrees)] = 0.0
-    node_degrees[np.isnan(node_degrees)] = 0.0
-    degree_matrix = sparse.diags(node_degrees, dtype=np.float32)
+    # degree matrix
+    node_degrees = np.array(adj_with_self_loops.sum(axis=1)).flatten()
 
-    adj = degree_matrix @ adj @ degree_matrix
-    return adj
+    # inverse square root of  degree matrix
+    inv_sqrt_degrees = np.power(node_degrees, -0.5)
+    inv_sqrt_degrees[np.isinf(inv_sqrt_degrees)] = 0.0  # Handle infinite values
+    inv_sqrt_degrees[np.isnan(inv_sqrt_degrees)] = 0.0  # Handle NaN values
 
+    # Create  diagonal matrix
+    inv_sqrt_degree_matrix = sparse.diags(inv_sqrt_degrees, dtype=np.float32)
 
-def convert_scipy_to_torch_sparse(matrix):
+    # Normalize
+    normalized_adj = inv_sqrt_degree_matrix @ adj_with_self_loops @ inv_sqrt_degree_matrix
+    return normalized_adj
+
+def reorder_node_id(node_id):
+    """Reorder the random node indexes to a sequential one"""
+    ids_ordered = {} # node_id : order
+    for order, raw_id in enumerate(node_id):  
+        ids_ordered[raw_id] = order
+
+    return ids_ordered
+
+def create_adjacency_matrix(edges_ordered, labels_enumerated): 
+    """Creates the adjacency matrix"""
+    num_edges = edges_ordered.shape[0] # number edges 
+
+    row_indices = edges_ordered[:, 0] # source 
+    col_indices = edges_ordered[:, 1] # target 
+
+    data = np.ones(num_edges, dtype=np.float32) # weights 
+    adj_shape = (labels_enumerated.shape[0], labels_enumerated.shape[0]) # dimension of matrix 
+    adj_mat = sparse.coo_matrix((data, (row_indices, col_indices)), shape=adj_shape)
+
+    # since undirected graph 
+    adj_symmetrical = adj_mat + adj_mat.T.multiply(adj_mat.T > adj_mat)
+    adj_normalized = normalize_adjacency(adj_symmetrical)
+    return adj_normalized
+
+def covert_to_pytorch_format(matrix):
+    """
+    Converts to pytorch format for computation after loading sparse to memory
+    """
     matrix_helper_coo = matrix.tocoo().astype('float32')
     data = torch.FloatTensor(matrix_helper_coo.data)
     rows = torch.LongTensor(matrix_helper_coo.row)
@@ -56,38 +87,47 @@ def convert_scipy_to_torch_sparse(matrix):
     matrix = torch.sparse.FloatTensor(indices, data, shape)
     return matrix
 
-
 def load_data(config):
     """Loads graph and put to sparse form for efficiancy"""
 
-    print("Loading Datase")
+    print("Loading CORA Dataset")
 
-    raw_nodes_data = np.genfromtxt(config.nodes_path, dtype="str")
-    raw_node_ids = raw_nodes_data[:, 0].astype('int32')  # unique identifier of each node
-    raw_node_labels = raw_nodes_data[:, -1]
-    labels_enumerated = enumerate_labels(raw_node_labels)  # target labels as integers
-    node_features = sparse.csr_matrix(raw_nodes_data[:, 1:-1], dtype="float32")
+    # loading node information  
+    raw_data = np.genfromtxt(config.nodes_path, dtype="str")
+    node_id = raw_data[:, 0].astype('int32')  # unique identifier of each node
+    node_labels = raw_data[:, -1]
 
-    # load structure 
-    ids_ordered = {raw_id: order for order, raw_id in enumerate(raw_node_ids)}
-    raw_edges_data = np.genfromtxt(config.edges_path, dtype="int32")
-    edges_ordered = np.array(list(map(ids_ordered.get, raw_edges_data.flatten())),
-                             dtype='int32').reshape(raw_edges_data.shape)
+    # integer label tagging 
+    labels_enumerated = map_labels(node_labels)  
 
-    # load adjaency matrix    
-    adj = sparse.coo_matrix((np.ones(edges_ordered.shape[0]), (edges_ordered[:, 0], edges_ordered[:, 1])),
-                            shape=(labels_enumerated.shape[0], labels_enumerated.shape[0]),
-                            dtype=np.float32)
+    # sparse matrix representation 
+    node_features = sparse.csr_matrix(raw_data[:, 1:-1], dtype="float32") # take the word features from w1 - w1433 
     
-    # Make the adjacency matrix symmetric
-    adj = adj + adj.T.multiply(adj.T > adj)
-    adj = normalize_adjacency(adj)
+    # redefine ids 
+    ordered_ids = reorder_node_id(node_id)
+    
+    # loading edges information & transform nodeids to match ordered one 
+    raw_edges_data = np.genfromtxt(config.edges_path, dtype="int32")
 
-    # change to pytorch format 
+    edges_ordered_list = []
+    for edge in raw_edges_data:
+        ordered_edge = [ordered_ids[edge[0]], ordered_ids[edge[1]]]
+        edges_ordered_list.append(ordered_edge)
+
+    edges_ordered = np.array(edges_ordered_list, dtype='int32') # example o/p of edges: array([[0, 1], [1, 2]]
+
+    adj_normalized = create_adjacency_matrix(edges_ordered, labels_enumerated)
     features = torch.FloatTensor(node_features.toarray())
     labels = torch.LongTensor(labels_enumerated)
-    adj = convert_scipy_to_torch_sparse(adj)
+    adj = covert_to_pytorch_format(adj_normalized)
 
-    print("Dataset loaded")
+    print("Loaded CORA Dataset")
 
     return features, labels, adj, edges_ordered
+
+
+if __name__ == "__main__": 
+    from args import config 
+    features, labels, adj, edges_orderded = load_data(config)
+
+    print ("Features",features, "labels", labels, "adj", adj, "edges_ordered", edges_orderded)
