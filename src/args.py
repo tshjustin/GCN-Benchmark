8f48f688e5352873c81cdc6c821755568de9235a
tshjustin <j.tann2305@gmail.com>
2024-09-28 12:08:42 +0800

    added EDA

--- src/args.py
+++ src/args.py
@@ -3,16 +3,16 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 
 # Model choice
-parser.add_argument("--model", type=str, choices=["GAT", "GCN"], default="GCN")
+parser.add_argument("--model", type=str, choices=["GAT", "GCN"], default="GAT")
 
 # Dataset choice
-parser.add_argument("--dataset", type=str, choices=["SQU", "CORA"], default="SQU")
+parser.add_argument("--dataset", type=str, choices=["SQU", "CORA"], default="CORA")
 
 # For GAT 
-parser.add_argument("--num_heads", type=int, nargs='+', default=[4]) # [4,8,16]
+parser.add_argument("--num_heads", type=int, nargs='+', default=[4,8,16]) # [4,8,16]
 
 # Model Parameters (For both GAT & GCN)
-parser.add_argument("--num_layers", type=int, nargs='+', default=[2,3,4]) # [2,3,4] - For the sake of less messy code, just manually change the varying variable here, 
+parser.add_argument("--num_layers", type=int, nargs='+', default=[2]) # [2,3,4] - For the sake of less messy code, just manually change the varying variable here, 
 parser.add_argument("--hidden_dim", type=int, nargs='+', default=[16]) #  [16,32,64] - Constant values (to ensure other variables are constant) are denoted with a list 
 parser.add_argument("--dropout", type=float, nargs='+', default=[0.5])
 parser.add_argument("--use-bias", type=bool, default=True)
